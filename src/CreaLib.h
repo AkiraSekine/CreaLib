97adcfa440e353df3fa3df0b1b17328f6ecaa108
AkiraSekine <ekusika834@gmail.com>
2018-11-27 16:17:36 +0900

    ヘッダファイルの追加

---
+++ src/CreaLib.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/// <summary>
+/// CreaLibのバージョン
+/// </summary>
+#define VERSION "1.0"
+/// <summary>
+/// CreaLibのバージョン
+/// </summary>
+#define W_VERSION L"1.0"
+
+namespace CreaLib
+{
+
+}
